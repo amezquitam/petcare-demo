8bfb8c40d6add24c7b2cae1bfcae3a669b5ab05b
amezquitam <amezquitamiguel7@gmail.com>
2024-05-19 14:44:27 -0500

    fixing-function-purpose

--- src/main/java/com/example/petcaredemo/repository/PetRepository.java
+++ src/main/java/com/example/petcaredemo/repository/PetRepository.java
@@ -2,9 +2,12 @@ package com.example.petcaredemo.repository;
 
 
 import com.example.petcaredemo.entity.Pet;
+import com.example.petcaredemo.entity.PetOwner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PetRepository extends JpaRepository<Pet, Long> {
-    Pet findPetByPetOwnerId(Long id);
+    List<Pet> findPetsByOwner(PetOwner owner);
 
 }
